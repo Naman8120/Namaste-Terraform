eea8644e9b8648a96c0785a951606f5ae87fc2c6
Naman Sharma <106026798+Naman8120@users.noreply.github.com>
2023-02-02 15:59:05 +0530

    Update first-ec2.tf

--- Part A - Deploying Infrastructure with Terraform/first-ec2.tf
+++ Part A - Deploying Infrastructure with Terraform/first-ec2.tf
@@ -1,3 +1,12 @@
+Important Note--
+Note that every region has a different AMI ID. The AMI ID's keeps on changing so make sure you use the latest AMI ID from the AWS console similar to the way it is shown in the video.
+
+Documentation Referred:
+https://registry.terraform.io/
+
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+
 provider "aws"
 {
   region = "us-west-2"
@@ -10,3 +19,9 @@ Resouce "aws_instance" "Ubuntu-Machine"
   ami = "ami-082b5a644766e0e6f"
   instance_type = "t2.micro"
 }
+
+
+Commands:
+terraform init
+terraform plan
+terraform apply
